6182617a3bbd22eab7abc8198a3cab31f1c2fdb2
dee-rius <darkwahdarius10@gmail.com>
2025-05-02 23:28:01 +0100

    updated js and css files_2025-Q2
    implemented checking for numbers of words and characters as well as displaying instances of user's chosen word

--- main.js
+++ main.js
@@ -1,4 +1,75 @@
-const textarea = document.querySelector("textarea");
+const wordToCountUserInput = document.getElementById("word-to-count-input");
+let wordToCount = "";
 
-textarea.addEventListener('change', () => {console.log(textarea.value.split(" "));});
+const countButton = document.getElementById("count-button");
+countButton.addEventListener('click', countInstancesOfWord);
+
+const userTextInput = document.getElementById("text-input");
+userTextInput.addEventListener("change", count)
+
+const outputTexts = Array.from(document.getElementsByClassName("output-text"));
+
+let splitByWords = [];
+let numOfWords = 0;
+
+let splitByCharacters = [];
+let numofCharacters = 0;
+let instancesOfChosenWord = 0;
+
+let storedInputDetails = {
+    storedWordToCountInput: "",
+    storedTextInput: "",
+
+    storedNumOfWords: "...",
+    storedNumOfCharacters: "...",
+    storedInstancesOfChosenWord: "..."
+}
+
+function countInstancesOfWord(){
+    splitByWords = userTextInput.value.split(" ");
+    wordToCount = wordToCountUserInput.value;
+    instancesOfChosenWord = 0;
+
+    for(let word of splitByWords){
+        if(word.toLowerCase() == wordToCount.toLowerCase()){
+            instancesOfChosenWord ++;
+        }
+    }
+
+    for(let outputText of outputTexts){
+        if(outputText.id == "instances-of-chosen-word"){
+            outputText.textContent = instancesOfChosenWord;
+        }
+    }
+}
+
+function count(){
+    numOfWords = 0;
+    numofCharacters = 0;
+
+    splitByWords = userTextInput.value.split(" ");
+    splitByCharacters = userTextInput.value.split("");
+
+    for(let word of splitByWords){
+        numOfWords ++;
+    }
+
+    for(let character of splitByCharacters){
+        numofCharacters ++;
+    }
+
+    for(let outputText of outputTexts){
+        if(outputText.id == "number-of-words")
+        {
+            outputText.textContent = numOfWords;
+        }
+        else if(outputText.id == "number-of-characters"){
+            outputText.textContent = numofCharacters;
+        }
+    }
+}
+
+function storeDetails(){
+
+}
 
